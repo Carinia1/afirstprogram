ab568ddea6fb2b60047242253cccc973c0b738d1
Karina Smardzewski <140887892+Carinia1@users.noreply.github.com>
2023-11-16 14:44:22 +0100

    Add files via upload

---
+++ Main06.java
@@ -0,0 +1,9 @@
+package pl.coderslab.afirstprogram;
+
+public class Main06 {
+
+	public static void main(String[] args) {
+//		//System.out.prinln("Hello world");
+		System.out.println("Hello world");
+	}
+}
